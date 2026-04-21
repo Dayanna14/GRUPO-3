bec75b83a2fc1ef537fd44c1a494d1e18bdfe52f
josstein09 <U202314290@upc.edu.pe>
2026-04-21 00:01:28 -0500

    Entidad Formulario agregada (falta el método uptade)

---
+++ src/main/java/com/avance/avancetb/repositories/IFormularioRepository.java
@@ -0,0 +1,9 @@
+package com.avance.avancetb.repositories;
+
+import com.avance.avancetb.entities.Formulario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IFormularioRepository extends JpaRepository<Formulario, Integer> {
+}
