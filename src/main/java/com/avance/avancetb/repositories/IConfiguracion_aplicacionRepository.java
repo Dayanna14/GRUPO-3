b39e7dc5e495a74df8a7c1b8472ea143af7e9510
josstein09 <U202314290@upc.edu.pe>
2026-04-29 23:14:37 -0500

    conf_aplicacion eliminada

--- src/main/java/com/avance/avancetb/repositories/IConfiguracion_aplicacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.avance.avancetb.repositories;
-
-import com.avance.avancetb.entities.Configuracion_aplicacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IConfiguracion_aplicacionRepository extends JpaRepository<Configuracion_aplicacion, Integer> {
-}
